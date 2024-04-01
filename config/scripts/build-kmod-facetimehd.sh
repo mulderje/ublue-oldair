8c9a2008da23da52a6499b9f829b1bfc86863bf4
Jon Mulder <jon.e.mulder@gmail.com>
2024-04-01 14:34:35 -0400

    Clean up build/run script for another attempt.

--- config/scripts/build-kmod-facetimehd.sh
+++ config/scripts/build-kmod-facetimehd.sh
@@ -2,20 +2,28 @@
 
 set -oeux pipefail
 
-export KERNEL_NAME="kernel"
-mkdir -p /tmp /var/tmp && \
+mkdir -p /tmp /var/tmp
 chmod -R 1777 /var/tmp
 
-wget https://copr.fedorainfracloud.org/coprs/mulderje/facetimehd-kmod/repo/fedora-$(rpm -E %fedora)/mulderje-facetimehd-kmod-fedora-$(rpm -E %fedora).repo -O /etc/yum.repos.d/_copr_mulderje-facetimehd-kmod.repo
-
 ARCH="$(rpm -E '%_arch')"
-KERNEL="$(rpm -q "${KERNEL_NAME}" --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
+KERNEL="$(rpm -q "${KERNEL_NAME:-kernel}" --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
 RELEASE="$(rpm -E '%fedora')"
+
+if [[ "${RELEASE}" -ge 40 ]]; then
+    COPR_RELEASE="rawhide"
+else
+    COPR_RELEASE="${RELEASE}"
+fi
+
+echo "$ARCH $KERNEL $RELEASE"
+wget "https://copr.fedorainfracloud.org/coprs/mulderje/facetimehd-kmod/repo/fedora-${COPR_RELEASE}/mulderje-facetimehd-kmod-fedora-${COPR_RELEASE}.repo" -O /etc/yum.repos.d/_copr_mulderje-facetimehd-kmod.repo
 
 ### BUILD facetimehd (succeed or fail-fast with debug output)
 rpm-ostree install \
-    akmod-facetimehd-*.fc${RELEASE}.${ARCH} \
+    "akmod-facetimehd-*.fc${RELEASE}.${ARCH}" \
     facetimehd-firmware
 akmods --force --kernels "${KERNEL}" --kmod facetimehd
-modinfo /usr/lib/modules/${KERNEL}/extra/facetimehd/facetimehd.ko.xz > /dev/null \
+modinfo "/usr/lib/modules/${KERNEL}/extra/facetimehd/facetimehd.ko.xz"sa > /dev/null \
 || (find /var/cache/akmods/facetimehd/ -name \*.log -print -exec cat {} \; && exit 1)
+
+rm -f /etc/yum.repos.d/_copr_mulderje-facetimehd-kmod.repo
