ea9b703f352ef04a38a977076a2d421b51dd499c
Jon Mulder <jon.e.mulder@gmail.com>
2024-03-31 20:32:56 -0400

    Change facetimehd kmod to a script for building.

---
+++ config/scripts/build-kmod-facetimehd.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+set -oeux pipefail
+
+
+ARCH="$(rpm -E '%_arch')"
+KERNEL="$(rpm -q "${KERNEL_NAME}" --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
+RELEASE="$(rpm -E '%fedora')"
+
+
+### BUILD facetimehd (succeed or fail-fast with debug output)
+rpm-ostree install \
+    akmod-facetimehd-*.fc${RELEASE}.${ARCH} \
+    facetimehd-firmware
+akmods --force --kernels "${KERNEL}" --kmod facetimehd
+modinfo /usr/lib/modules/${KERNEL}/extra/facetimehd/facetimehd.ko.xz > /dev/null \
+|| (find /var/cache/akmods/facetimehd/ -name \*.log -print -exec cat {} \; && exit 1)
