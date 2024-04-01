1551c8e6dfc212aa8d3ef5509977002863966759
Jon Mulder <jon.e.mulder@gmail.com>
2024-04-01 14:53:46 -0400

    Clean up build script issues.

--- config/scripts/build-kmod-facetimehd.sh
+++ config/scripts/build-kmod-facetimehd.sh
@@ -15,15 +15,13 @@ else
     COPR_RELEASE="${RELEASE}"
 fi
 
-echo "$ARCH $KERNEL $RELEASE"
 wget "https://copr.fedorainfracloud.org/coprs/mulderje/facetimehd-kmod/repo/fedora-${COPR_RELEASE}/mulderje-facetimehd-kmod-fedora-${COPR_RELEASE}.repo" -O /etc/yum.repos.d/_copr_mulderje-facetimehd-kmod.repo
 
 ### BUILD facetimehd (succeed or fail-fast with debug output)
 rpm-ostree install \
-    "akmod-facetimehd-*.fc${RELEASE}.${ARCH}" \
-    facetimehd-firmware
+    "akmod-facetimehd-*.fc${RELEASE}.${ARCH}"
 akmods --force --kernels "${KERNEL}" --kmod facetimehd
-modinfo "/usr/lib/modules/${KERNEL}/extra/facetimehd/facetimehd.ko.xz"sa > /dev/null \
+modinfo "/usr/lib/modules/${KERNEL}/extra/facetimehd/facetimehd.ko.xz" > /dev/null \
 || (find /var/cache/akmods/facetimehd/ -name \*.log -print -exec cat {} \; && exit 1)
 
 rm -f /etc/yum.repos.d/_copr_mulderje-facetimehd-kmod.repo
