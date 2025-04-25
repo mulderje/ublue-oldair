171a4de328bcf3564149b506d876f2ce602f82dd
Jon Mulder <jon.e.mulder@gmail.com>
2025-04-25 05:42:47 -0400

    Update build-kmod-wl.sh
    Automatic "yes" for install repos

--- files/scripts/build-kmod-wl.sh
+++ files/scripts/build-kmod-wl.sh
@@ -6,7 +6,9 @@ ARCH="$(rpm -E '%_arch')"
 KERNEL="$(rpm -q "${KERNEL_NAME:-kernel}" --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
 RELEASE="$(rpm -E '%fedora')"
 
-dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+dnf install -y \
+  https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
+  https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
 ### BUILD wl (succeed or fail-fast with debug output)
 dnf install -y \
