acc5cdeb2216ea7a52a138a0750039e48a8e6175
Jon Mulder <jon.e.mulder@gmail.com>
2025-04-25 15:28:08 -0400

    [cleanup] enable/disable terra and rpmfusion repos instead of installing new ones

--- files/scripts/build-kmod-wl.sh
+++ files/scripts/build-kmod-wl.sh
@@ -6,9 +6,8 @@ ARCH="$(rpm -E '%_arch')"
 KERNEL="$(rpm -q "${KERNEL_NAME:-kernel}" --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
 RELEASE="$(rpm -E '%fedora')"
 
-dnf5 install -y \
-  https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-${RELEASE}.noarch.rpm \
-  https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-${RELEASE}.noarch.rpm
+dnf5 -y config-manager setopt "terra".enabled=true
+sed -i 's@enabled=0@enabled=1@g' /etc/yum.repos.d/rpmfusion-*.repo
 
 ### BUILD wl (succeed or fail-fast with debug output)
 dnf5 install -y \
@@ -16,3 +15,6 @@ dnf5 install -y \
 akmods --force --kernels "${KERNEL}" --kmod wl
 modinfo /usr/lib/modules/${KERNEL}/extra/wl/wl.ko.xz >/dev/null ||
   (find /var/cache/akmods/wl/ -name \*.log -print -exec cat {} \; && exit 1)
+
+dnf5 -y config-manager setopt "terra".enabled=false
+sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/rpmfusion-*.repo
