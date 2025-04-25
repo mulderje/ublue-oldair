82836333bf1f77a0d56b61e44fde23b4efda5d85
Jon Mulder <jon.e.mulder@gmail.com>
2025-04-24 22:53:21 -0400

    [wl] give kernel a default

--- files/scripts/build-kmod-wl.sh
+++ files/scripts/build-kmod-wl.sh
@@ -2,15 +2,13 @@
 
 set -oeux pipefail
 
-
 ARCH="$(rpm -E '%_arch')"
-KERNEL="$(rpm -q "${KERNEL_NAME}" --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
+KERNEL="$(rpm -q "${KERNEL_NAME:-kernel}" --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
 RELEASE="$(rpm -E '%fedora')"
-
 
 ### BUILD wl (succeed or fail-fast with debug output)
 dnf install -y \
-    akmod-wl-*.fc${RELEASE}.${ARCH}
+  akmod-wl-*.fc${RELEASE}.${ARCH}
 akmods --force --kernels "${KERNEL}" --kmod wl
-modinfo /usr/lib/modules/${KERNEL}/extra/wl/wl.ko.xz > /dev/null \
-|| (find /var/cache/akmods/wl/ -name \*.log -print -exec cat {} \; && exit 1)
+modinfo /usr/lib/modules/${KERNEL}/extra/wl/wl.ko.xz >/dev/null ||
+  (find /var/cache/akmods/wl/ -name \*.log -print -exec cat {} \; && exit 1)
