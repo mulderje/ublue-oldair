142cc85877326870904576540e6cd2afc0406f2f
Jon Mulder <jon.e.mulder@gmail.com>
2025-04-25 16:12:25 -0400

    [fixup] use kmod-wl explicitly

--- files/scripts/build-kmod-wl.sh
+++ files/scripts/build-kmod-wl.sh
@@ -12,7 +12,7 @@ sed -i 's@enabled=0@enabled=1@g' /etc/yum.repos.d/rpmfusion-*.repo
 
 ### BUILD wl (succeed or fail-fast with debug output)
 dnf5 install -y \
-  akmod-wl-*.fc${RELEASE}.${ARCH}
+  kmod-wl.fc${RELEASE}.${ARCH}
 akmods --force --kernels "${KERNEL}" --kmod wl
 modinfo /usr/lib/modules/${KERNEL}/extra/wl/wl.ko.xz >/dev/null ||
   (find /var/cache/akmods/wl/ -name \*.log -print -exec cat {} \; && exit 1)
