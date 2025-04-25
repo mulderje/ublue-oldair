b90a2f0647e52d8d9c3eef23706ce5a3315bb009
Jon Mulder <jon.e.mulder@gmail.com>
2025-04-25 15:53:28 -0400

    [fixup] disable terra repo for wl
    Terra repo seems to not be applying the last two patches
    
    Link: https://github.com/terrapkg/packages/tree/frawhide/anda/system/wl-kmod
    Link: https://github.com/rpmfusion/wl-kmod/tree/master

--- files/scripts/build-kmod-wl.sh
+++ files/scripts/build-kmod-wl.sh
@@ -6,7 +6,8 @@ ARCH="$(rpm -E '%_arch')"
 KERNEL="$(rpm -q "${KERNEL_NAME:-kernel}" --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
 RELEASE="$(rpm -E '%fedora')"
 
-dnf5 -y config-manager setopt "terra".enabled=true
+# TODO: fix terra wl spec
+# dnf5 -y config-manager setopt "terra".enabled=true
 sed -i 's@enabled=0@enabled=1@g' /etc/yum.repos.d/rpmfusion-*.repo
 
 ### BUILD wl (succeed or fail-fast with debug output)
@@ -16,5 +17,6 @@ akmods --force --kernels "${KERNEL}" --kmod wl
 modinfo /usr/lib/modules/${KERNEL}/extra/wl/wl.ko.xz >/dev/null ||
   (find /var/cache/akmods/wl/ -name \*.log -print -exec cat {} \; && exit 1)
 
-dnf5 -y config-manager setopt "terra".enabled=false
+# TODO: fix terra wl spec
+# dnf5 -y config-manager setopt "terra".enabled=false
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/rpmfusion-*.repo
