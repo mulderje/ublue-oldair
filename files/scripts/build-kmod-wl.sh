6b1d58eefff4fbaa47f09762c0e0536dc3485557
Jon Mulder <jon.e.mulder@gmail.com>
2025-04-25 15:03:01 -0400

    [cleanup] switch dnf to dnf5 explicitly

--- files/scripts/build-kmod-wl.sh
+++ files/scripts/build-kmod-wl.sh
@@ -6,12 +6,12 @@ ARCH="$(rpm -E '%_arch')"
 KERNEL="$(rpm -q "${KERNEL_NAME:-kernel}" --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
 RELEASE="$(rpm -E '%fedora')"
 
-dnf install -y \
+dnf5 install -y \
   https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-${RELEASE}.noarch.rpm \
   https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-${RELEASE}.noarch.rpm
 
 ### BUILD wl (succeed or fail-fast with debug output)
-dnf install -y \
+dnf5 install -y \
   akmod-wl-*.fc${RELEASE}.${ARCH}
 akmods --force --kernels "${KERNEL}" --kmod wl
 modinfo /usr/lib/modules/${KERNEL}/extra/wl/wl.ko.xz >/dev/null ||
