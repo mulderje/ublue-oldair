23dd0089ee0189d1f1e96ce1bd410bb39c67b491
Jon Mulder <jon.e.mulder@gmail.com>
2025-04-24 23:27:34 -0400

    Update build-kmod-wl.sh

--- files/scripts/build-kmod-wl.sh
+++ files/scripts/build-kmod-wl.sh
@@ -6,6 +6,8 @@ ARCH="$(rpm -E '%_arch')"
 KERNEL="$(rpm -q "${KERNEL_NAME:-kernel}" --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
 RELEASE="$(rpm -E '%fedora')"
 
+dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
 ### BUILD wl (succeed or fail-fast with debug output)
 dnf install -y \
   akmod-wl-*.fc${RELEASE}.${ARCH}
