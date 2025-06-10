48e5016610e2d97d7576184617bee6bfb81343a8
Jon Mulder <jon.e.mulder@gmail.com>
2025-06-09 22:50:34 -0400

    wl-kmod: switch to using --enable-repo dnf option

--- files/scripts/build-kmod-wl.sh
+++ files/scripts/build-kmod-wl.sh
@@ -6,19 +6,9 @@ ARCH="$(rpm -E '%_arch')"
 KERNEL="$(rpm -q "${KERNEL_NAME:-kernel}" --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
 RELEASE="$(rpm -E '%fedora')"
 
-# TODO: fix terra wl spec
-# dnf5 -y config-manager setopt "terra".enabled=true
-dnf5 install -y \
-  https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-${RELEASE}.noarch.rpm \
-  https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-${RELEASE}.noarch.rpm
-
 ### BUILD wl (succeed or fail-fast with debug output)
-dnf5 install -y \
+dnf5 install -y --enable-repo="rpmfusion-nonfree" \
   akmod-wl-*.fc${RELEASE}.${ARCH}
 akmods --force --kernels "${KERNEL}" --kmod wl
 modinfo /usr/lib/modules/${KERNEL}/extra/wl/wl.ko.xz >/dev/null ||
   (find /var/cache/akmods/wl/ -name \*.log -print -exec cat {} \; && exit 1)
-
-# TODO: fix terra wl spec
-# dnf5 -y config-manager setopt "terra".enabled=false
-sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/rpmfusion-*.repo
